fd58b6e8811d93f2bb624dda7b33d5dfd6efbe06
unknown <math2.0@hotmail.com>
2026-03-24 07:42:44 -0300

    shorter intervals for grad acc

--- start_training.py
+++ start_training.py
@@ -34,7 +34,7 @@ from metrics_logger import LossMonitor, MetricsLogger
 
 load_dotenv()
 
-CHECKPOINT_INTERVAL = 1000
+CHECKPOINT_INTERVAL = 100
 SORT_BUFFER_SIZE = 1000
 PREFETCH_SIZE = 16
 PHASE_STEP = 200000
