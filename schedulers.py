ca544af213a47f514198155dc87ba8f76402ba4c
unknown <math2.0@hotmail.com>
2026-03-23 23:07:25 -0300

    re-arrange phases

--- schedulers.py
+++ schedulers.py
@@ -1,34 +1,44 @@
 import optax
+from train_local import BATCH_SIZE, MAX_SEQ_LEN 
+
+def calc_sched():
+    tokens_per_step = BATCH_SIZE * MAX_SEQ_LEN
+    TARGET_TOKENS = 6000000
+    total_steps = TARGET_TOKENS // tokens_per_step
+    return total_steps
+
+WARMUP = calc_sched() // 3
+DECAY = WARMUP * 2
 
 learning_schedule = optax.warmup_cosine_decay_schedule(
     init_value=1e-6, 
     peak_value=2e-4,
-    warmup_steps=500, 
-    decay_steps=1000, 
+    warmup_steps=WARMUP, 
+    decay_steps=DECAY, 
     end_value=1e-5
 )
 
 ponder_lambda_schedule = optax.warmup_cosine_decay_schedule(
     init_value=0.0, 
     peak_value=0.0, 
-    warmup_steps=500, 
-    decay_steps=1000, 
+    warmup_steps=WARMUP, 
+    decay_steps=DECAY, 
     end_value=1e-4
 )
 
 forget_lambda_schedule = optax.warmup_cosine_decay_schedule(
     init_value=0.0, 
     peak_value=0.0, 
-    warmup_steps=500, 
-    decay_steps=1000, 
+    warmup_steps=WARMUP, 
+    decay_steps=DECAY, 
     end_value=4e-3
 )
 
 diversity_lambda_schedule = optax.linear_schedule(
     init_value=0.0,
     end_value=0.12,
-    transition_steps=500,
-    transition_begin=1000,
+    transition_steps=WARMUP,
+    transition_begin=DECAY,
 )
 
 optimizer_chain = optax.chain(
