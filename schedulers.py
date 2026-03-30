aaf20b6d93dec52b080b83a169b54713975b6a68
unknown <math2.0@hotmail.com>
2026-03-29 23:18:06 -0300

    -_-

--- schedulers.py
+++ schedulers.py
@@ -4,7 +4,7 @@ learning_schedule = optax.warmup_cosine_decay_schedule(
     init_value=1e-6, 
     peak_value=2e-4,
     warmup_steps=1000, 
-    decay_steps=2000, 
+    decay_steps=3000, 
     end_value=1e-5
 )
 
@@ -12,21 +12,21 @@ ponder_lambda_schedule = optax.linear_schedule(
     init_value=0.0,
     end_value=1e-4,
     transition_steps=1000,
-    transition_begin=2000,
+    transition_begin=3000,
 )
 
 forget_lambda_schedule = optax.linear_schedule(
     init_value=0.0,
     end_value=4e-3,
     transition_steps=1000,
-    transition_begin=2000,
+    transition_begin=3000,
 )
 
 diversity_lambda_schedule = optax.linear_schedule(
     init_value=0.0,
     end_value=0.12,
     transition_steps=1000,
-    transition_begin=2000,
+    transition_begin=3000,
 )
 
 optimizer_chain = optax.chain(
