7adc95af77384807b227503cef72a12105358489
unknown <math2.0@hotmail.com>
2026-03-29 23:22:27 -0300

    cosine schedule

--- schedulers.py
+++ schedulers.py
@@ -3,30 +3,33 @@ import optax
 learning_schedule = optax.warmup_cosine_decay_schedule(
     init_value=1e-6, 
     peak_value=2e-4,
-    warmup_steps=300, 
+    warmup_steps=500, 
     decay_steps=1000, 
     end_value=1e-5
 )
 
-ponder_lambda_schedule = optax.linear_schedule(
-    init_value=0.0,
-    end_value=1e-4,
-    transition_steps=1000,
-    transition_begin=3000,
+ponder_lambda_schedule = optax.warmup_cosine_decay_schedule(
+    init_value=0.0, 
+    peak_value=0.0, 
+    warmup_steps=500, 
+    decay_steps=1000, 
+    end_value=1e-4
 )
 
-forget_lambda_schedule = optax.linear_schedule(
-    init_value=0.0,
-    end_value=4e-3,
-    transition_steps=1000,
-    transition_begin=3000,
+forget_lambda_schedule = optax.warmup_cosine_decay_schedule(
+    init_value=0.0, 
+    peak_value=0.0, 
+    warmup_steps=500, 
+    decay_steps=1000, 
+    end_value=4e-3
 )
 
-diversity_lambda_schedule = optax.linear_schedule(
-    init_value=0.0,
-    end_value=0.12,
-    transition_steps=1000,
-    transition_begin=3000,
+diversity_lambda_schedule = optax.warmup_cosine_decay_schedule(
+    init_value=0.0, 
+    peak_value=0.0, 
+    warmup_steps=500, 
+    decay_steps=1000, 
+    end_value=0.12
 )
 
 optimizer_chain = optax.chain(
