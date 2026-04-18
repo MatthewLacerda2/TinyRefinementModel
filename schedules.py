2344638152d7e4a938c49e05c22814205d6e8f36
unknown <math2.0@hotmail.com>
2026-04-18 15:13:55 -0300

    LATEST GOOD RUN

--- schedules.py
+++ schedules.py
@@ -5,7 +5,7 @@ DECAY_STEPS = 60000
 
 learning_schedule = optax.warmup_cosine_decay_schedule(
     init_value=1e-5,
-    peak_value=2e-4,
+    peak_value=1e-4,
     warmup_steps=WARMUP_STEPS, 
     decay_steps=DECAY_STEPS, 
     end_value=1e-6
