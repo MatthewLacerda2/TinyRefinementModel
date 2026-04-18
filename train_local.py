75faf77db3f5341d29af7d5da01fa9427f017f3d
unknown <math2.0@hotmail.com>
2026-04-18 16:13:23 -0300

    split weights

--- train_local.py
+++ train_local.py
@@ -236,8 +236,8 @@ class UniversalReasoner(nnx.Module):
 
         self.seq_norm = nnx.RMSNorm(latent_dim, rngs=rngs, dtype=dtype)
         
-        self.encoder_stack = BlockStack(1, latent_dim, num_heads=NUM_HEADS, rngs=rngs, dtype=dtype, share_weights=False)
-        self.decoder_stack = BlockStack(1, latent_dim, num_heads=NUM_HEADS, rngs=rngs, dtype=dtype, share_weights=False)
+        self.encoder_stack = BlockStack(num_blocks, latent_dim, num_heads=NUM_HEADS, rngs=rngs, dtype=dtype, share_weights=False)
+        self.decoder_stack = BlockStack(num_blocks, latent_dim, num_heads=NUM_HEADS, rngs=rngs, dtype=dtype, share_weights=False)
         self.reasoning_stack = BlockStack(num_blocks, latent_dim, num_heads=NUM_HEADS, rngs=rngs, dtype=dtype, share_weights=False)
 
         self.meta_proj = nnx.Linear(3, latent_dim, rngs=rngs, dtype=dtype)
